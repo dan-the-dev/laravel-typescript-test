d14cc7ea0a391954d79fc6e85852bd5c14a19d20
dan-the-dev <daniele.scillia@gmail.com>
2022-12-18 01:53:33 +0100

    feat: final version of poc working as expected and packag publishable

--- resources/types/generated.d.ts
+++ resources/types/generated.d.ts
@@ -1,23 +1,19 @@
-declare namespace App.Custom {
-    export type Language = "en" | "fr" | "it";
-}
-declare namespace App.Http.Controllers.Test {
-    export type TestFormRequest = {
-        string: string;
-        int: number;
-        float: number;
-        language: App.Custom.Language;
-    };
-    export type TestRequest = {
-        string: string;
-        int: number;
-        float: number;
-        language: App.Custom.Language;
-    };
-    export type TestResponse = {
-        string: string;
-        int: number;
-        float: number;
-        language: App.Custom.Language;
-    };
-}
+export type Language = "en" | "fr" | "it";
+export type TestFormRequest = {
+    string: string;
+    int: number;
+    float: number;
+    language: Language;
+};
+export type TestRequest = {
+    string: string;
+    int: number;
+    float: number;
+    language: Language;
+};
+export type TestResponse = {
+    string: string;
+    int: number;
+    float: number;
+    language: Language;
+};
